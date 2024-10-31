828a974d605b10084e9339efbff6e40f8268107b
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2024-10-31 12:30:08 +0100

    Initial Release. Full functions and code.

---
+++ Configurable_Landfill/changes.lua
@@ -0,0 +1,17 @@
+--Change recipe
+data.raw.recipe["landfill"].ingredients = {{type = "item", name = "stone", amount = settings.startup["landfill-cost"].value }}
+data.raw.recipe["landfill"].results = {{type="item", name="landfill", amount= settings.startup["landfill-results"].value}}
+data.raw.recipe["landfill"].energy_required = settings.startup["landfill-energy"].value/10
+
+
+--Unlock landfill form start
+if settings.startup["landfill-unlocked-from-start"].value == true then
+  data.raw.technology["landfill"].unlocked = true
+  data.raw.technology["landfill"].hidden = true
+  data.raw.recipe["landfill"].enabled = true
+end
+
+--landfill stack size
+if not mods["BigBags2"] then
+data.raw.item["landfill"].stack_size = settings.startup["landfill-stack-size"].value
+end
---
+++ Configurable_Landfill/settings.lua
@@ -0,0 +1,44 @@
+--Landfill
+
+data:extend({
+    {
+        type = "bool-setting",
+        name = "landfill-unlocked-from-start",
+        setting_type = "startup",
+        default_value = false,
+        order = "b"
+    },
+    {
+        type = "int-setting",
+        name = "landfill-cost",
+        setting_type = "startup",
+        default_value = 50,
+        order = "d"
+    },
+    {
+        type = "int-setting",
+        name = "landfill-results",
+        setting_type = "startup",
+        default_value = 1,
+        order = "e"
+    },
+    {
+        type = "int-setting",
+        name = "landfill-energy",
+        setting_type = "startup",
+        default_value = 5,
+        order = "f"
+    },
+})
+if not mods["BigBags2"] then
+    data:extend({
+        {
+            type = "int-setting",
+            name = "landfill-stack-size",
+            setting_type = "startup",
+            default_value = 100,
+            minimum_value = 1,
+            order = "c"
+        }
+    })
+end
