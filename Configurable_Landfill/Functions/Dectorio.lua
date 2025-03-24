e38ac47eff9882aac90ab018c68ee3a8bdf0a6c9
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2025-03-24 18:16:55 +0100

    Dectorio Support P2

--- Configurable_Landfill/Functions/Dectorio.lua
+++ Configurable_Landfill/Functions/Dectorio.lua
@@ -6,4 +6,47 @@ if mods["Dectorio"] then
 	if SS["dectorio-concrete"].value == true then
 		data.raw.tile["dect-concrete-grid"].walking_speed_modifier = SS["DectConcreteGridSpeed"].value
 	end
+	if SS["dectorio-painted-concrete"].value == true then
+		data.raw.tile["acid-refined-concrete"].walking_speed_modifier = SS["ColouredRefinedConcreteSpeed"].value
+		data.raw.tile["black-refined-concrete"].walking_speed_modifier = SS["ColouredRefinedConcreteSpeed"].value
+		data.raw.tile["blue-refined-concrete"].walking_speed_modifier = SS["ColouredRefinedConcreteSpeed"].value
+		data.raw.tile["brown-refined-concrete"].walking_speed_modifier = SS["ColouredRefinedConcreteSpeed"].value
+		data.raw.tile["cyan-refined-concrete"].walking_speed_modifier = SS["ColouredRefinedConcreteSpeed"].value
+		data.raw.tile["green-refined-concrete"].walking_speed_modifier = SS["ColouredRefinedConcreteSpeed"].value
+		data.raw.tile["orange-refined-concrete"].walking_speed_modifier = SS["ColouredRefinedConcreteSpeed"].value
+		data.raw.tile["pink-refined-concrete"].walking_speed_modifier = SS["ColouredRefinedConcreteSpeed"].value
+		data.raw.tile["purple-refined-concrete"].walking_speed_modifier = SS["ColouredRefinedConcreteSpeed"].value
+		data.raw.tile["red-refined-concrete"].walking_speed_modifier = SS["ColouredRefinedConcreteSpeed"].value
+		data.raw.tile["yellow-refined-concrete"].walking_speed_modifier = SS["ColouredRefinedConcreteSpeed"].value
+
+		data.raw.tile["dect-paint-refined-danger-left"].walking_speed_modifier = SS["ColouredHazardRefinedConcreteSpeed"].value
+		data.raw.tile["dect-paint-refined-emergency-left"].walking_speed_modifier = SS["ColouredHazardRefinedConcreteSpeed"].value
+		data.raw.tile["dect-paint-refined-caution-left"].walking_speed_modifier = SS["ColouredHazardRefinedConcreteSpeed"].value
+		data.raw.tile["dect-paint-refined-radiation-left"].walking_speed_modifier = SS["ColouredHazardRefinedConcreteSpeed"].value
+		data.raw.tile["dect-paint-refined-defect-left"].walking_speed_modifier = SS["ColouredHazardRefinedConcreteSpeed"].value
+		data.raw.tile["dect-paint-refined-operations-left"].walking_speed_modifier = SS["ColouredHazardRefinedConcreteSpeed"].value
+		data.raw.tile["dect-paint-refined-safety-left"].walking_speed_modifier = SS["ColouredHazardRefinedConcreteSpeed"].value
+		data.raw.tile["dect-paint-danger-left"].walking_speed_modifier = SS["ColouredHazardConcreteSpeed"].value
+		data.raw.tile["dect-paint-emergency-left"].walking_speed_modifier = SS["ColouredHazardConcreteSpeed"].value
+		data.raw.tile["dect-paint-caution-left"].walking_speed_modifier = SS["ColouredHazardConcreteSpeed"].value
+		data.raw.tile["dect-paint-radiation-left"].walking_speed_modifier = SS["ColouredHazardConcreteSpeed"].value
+		data.raw.tile["dect-paint-defect-left"].walking_speed_modifier = SS["ColouredHazardConcreteSpeed"].value
+		data.raw.tile["dect-paint-operations-left"].walking_speed_modifier = SS["ColouredHazardConcreteSpeed"].value
+		data.raw.tile["dect-paint-safety-left"].walking_speed_modifier = SS["ColouredHazardConcreteSpeed"].value
+
+		data.raw.tile["dect-paint-refined-danger-right"].walking_speed_modifier = SS["ColouredHazardRefinedConcreteSpeed"].value
+		data.raw.tile["dect-paint-refined-emergency-right"].walking_speed_modifier = SS["ColouredHazardRefinedConcreteSpeed"].value
+		data.raw.tile["dect-paint-refined-caution-right"].walking_speed_modifier = SS["ColouredHazardRefinedConcreteSpeed"].value
+		data.raw.tile["dect-paint-refined-radiation-right"].walking_speed_modifier = SS["ColouredHazardRefinedConcreteSpeed"].value
+		data.raw.tile["dect-paint-refined-defect-right"].walking_speed_modifier = SS["ColouredHazardRefinedConcreteSpeed"].value
+		data.raw.tile["dect-paint-refined-operations-right"].walking_speed_modifier = SS["ColouredHazardRefinedConcreteSpeed"].value
+		data.raw.tile["dect-paint-refined-safety-right"].walking_speed_modifier = SS["ColouredHazardRefinedConcreteSpeed"].value
+		data.raw.tile["dect-paint-danger-right"].walking_speed_modifier = SS["ColouredHazardConcreteSpeed"].value
+		data.raw.tile["dect-paint-emergency-right"].walking_speed_modifier = SS["ColouredHazardConcreteSpeed"].value
+		data.raw.tile["dect-paint-caution-right"].walking_speed_modifier = SS["ColouredHazardConcreteSpeed"].value
+		data.raw.tile["dect-paint-radiation-right"].walking_speed_modifier = SS["ColouredHazardConcreteSpeed"].value
+		data.raw.tile["dect-paint-defect-right"].walking_speed_modifier = SS["ColouredHazardConcreteSpeed"].value
+		data.raw.tile["dect-paint-operations-right"].walking_speed_modifier = SS["ColouredHazardConcreteSpeed"].value
+		data.raw.tile["dect-paint-safety-right"].walking_speed_modifier = SS["ColouredHazardConcreteSpeed"].value
+	end
 end
--- Configurable_Landfill/settings.lua
+++ Configurable_Landfill/settings.lua
@@ -286,14 +286,35 @@ if mods["Dectorio"] then
             name = "DectWoodSpeed",
             setting_type = "startup",
             default_value = 1.2,
-            order = "5Ab"
+            order = "5Aa"
         },
         {
             type = "double-setting",
             name = "DectConcreteGridSpeed",
             setting_type = "startup",
             default_value = 1.4,
-            order = "5Aa"
+            order = "5Ab"
+        },
+        {
+            type = "double-setting",
+            name = "ColouredHazardConcreteSpeed",
+            setting_type = "startup",
+            default_value = 1.4,
+            order = "5Ac"
+        },
+        {
+            type = "double-setting",
+            name = "ColouredHazardRefinedConcreteSpeed",
+            setting_type = "startup",
+            default_value = 1.5,
+            order = "5Ad"
+        },
+        {
+            type = "double-setting",
+            name = "ColouredRefinedConcreteSpeed",
+            setting_type = "startup",
+            default_value = 1.5,
+            order = "5Ae"
         },
     })
 end
