2a0d7995286a255d7c202f25c8bdef176933f7cc
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2025-03-24 11:29:34 +0100

    Settings checks to avoid crashes.

--- Configurable_Landfill/Functions/Dectorio.lua
+++ Configurable_Landfill/Functions/Dectorio.lua
@@ -1,4 +1,9 @@
 local SS = settings.startup
-
-data.raw.tile["dect-wood-floor"].walking_speed_modifier = SS["DectWoodSpeed"].value
-data.raw.tile["dect-concrete-grid"].walking_speed_modifier = SS["DectConcreteGridSpeed"].value
+if mods["Dectorio"] then
+	if SS["dectorio-wood"].value == true then
+		data.raw.tile["dect-wood-floor"].walking_speed_modifier = SS["DectWoodSpeed"].value
+	end
+	if SS["dectorio-concrete"].value == true then
+		data.raw.tile["dect-concrete-grid"].walking_speed_modifier = SS["DectConcreteGridSpeed"].value
+	end
+end
