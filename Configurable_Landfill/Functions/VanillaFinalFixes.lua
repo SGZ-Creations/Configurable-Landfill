6860eec51b9260dab91692e77dd9da57a1d05514
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2025-08-05 15:28:58 +0200

    Gregtorio Comptaibility

--- Configurable_Landfill/Functions/VanillaFinalFixes.lua
+++ Configurable_Landfill/Functions/VanillaFinalFixes.lua
@@ -6,9 +6,19 @@ local Recipes = data.raw.recipe
 local SS = settings.startup
 
 --Unlock landfill from start
-if SS["landfill-unlocked-from-start"].value == true then
-    Recipes["landfill"].enabled = true
-    Tech["landfill"].research_trigger = {type ="craft-item", item ="landfill", amount = 1}
-    Tech["landfill"].unit = nil
-    Tech["landfill"].prerequisites = nil
+if mods["base"] then
+    if SS["landfill-unlocked-from-start"].value == true then
+        Recipes["landfill"].enabled = true
+        Tech["landfill"].research_trigger = {type ="craft-item", item ="landfill", amount = 1}
+        Tech["landfill"].unit = nil
+        Tech["landfill"].prerequisites = nil
+    end
+end
+if mods["Gregtorio"] then
+    if SS["landfill-unlocked-from-start"].value == true then
+        Recipes["landfill"].enabled = true
+        Tech["landfill"].research_trigger = {type ="craft-item", item ="landfill", amount = 1}
+        Tech["landfill"].unit = nil
+        Tech["landfill"].prerequisites = nil
+    end
 end
